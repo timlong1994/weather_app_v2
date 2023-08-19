f9c39889aa33069b82be809d512495f979c3d388
timlong1994 <67029684+timlong1994@users.noreply.github.com>
2023-08-19 23:01:25 +0800

    Implement Error Message display

---
+++ weather_app/src/components/ErrorMessage.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <p id="error_message">Please enter a valid location</p>
+</template>
+
+<style scoped>
+#error_message {
+    margin: 10px;
+    font-size: 18pt;
+    text-align: center;
+}
+
+/* Tablet Breakpoint */
+@media only screen and (min-width: 600px) {
+    body {
+        font-size: 22pt;
+        height: 100%;
+        justify-content: center;
+    }
+}
+
+/* Desktop Breakpoint */
+@media only screen and (min-width: 850px) {
+    #error_message {
+        text-align: center;
+        grid-row: 2;
+        grid-column: 1/11;
+    }
+}
+
+</style>
