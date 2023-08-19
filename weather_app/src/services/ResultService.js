b5332c9bf16f9f1c3c0712572fda15659917a6bf
timlong1994 <67029684+timlong1994@users.noreply.github.com>
2023-08-19 21:56:58 +0800

    Implement API Call

--- weather_app/src/services/ResultService.js
+++ weather_app/src/services/ResultService.js
@@ -1,19 +1,16 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'http://api.weatherapi.com/v1/current.json',
-    withCredentials: false
+    baseURL: 'http://api.weatherapi.com/v1/current.json?key=1c5a98bf8b524cf5bb3185319230507&q=',
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
 })
 
 export default {
-    getWeather() {
-        const result = apiClient.get({
-            params: {
-                key: '1c5a98bf8b524cf5bb3185319230507',
-                q: 'Singapore',
-            }
-        })
-
-        console.log(result)
+    getWeather(location) {
+        return apiClient.get(location)
     }
 }
