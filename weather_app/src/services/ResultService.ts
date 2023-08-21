f4fedf33c9493b9833a8a8ef3c655507a51fdca6
timlong1994 <67029684+timlong1994@users.noreply.github.com>
2023-08-21 23:24:25 +0800

    Add type annotation to getWeather() function

--- weather_app/src/services/ResultService.ts
+++ weather_app/src/services/ResultService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-import type { AxiosInstance } from 'axios'
+import type { AxiosInstance, AxiosResponse } from 'axios'
 
 const apiClient: AxiosInstance = axios.create({
     baseURL: 'http://api.weatherapi.com/v1/current.json?key=1c5a98bf8b524cf5bb3185319230507&q=',
@@ -11,7 +11,7 @@ const apiClient: AxiosInstance = axios.create({
 })
 
 export default {
-    getWeather(location: string) {
+    getWeather(location: string): Promise<AxiosResponse> {
         return apiClient.get(location)
     }
 }
