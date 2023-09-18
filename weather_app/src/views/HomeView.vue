0e27262d9111cf0be300d2cd5f11eea2bc8bd4fd
timlong1994 <67029684+timlong1994@users.noreply.github.com>
2023-09-18 14:54:33 +0800

    Implement error messages for specific errors thrown by API method

--- weather_app/src/views/HomeView.vue
+++ weather_app/src/views/HomeView.vue
@@ -1,46 +1,40 @@
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref } from 'vue'
 
 import SearchBar from '../components/SearchBar.vue'
 import SearchResult from '../components/SearchResult.vue'
 import ResultService from '../services/ResultService'
 
-import InvalidLocationError from '../services/ResultService'
-
-
-import type { Ref } from "vue"
+import type { Ref } from 'vue'
 
 import type { Weather } from '../types'
 
-const results: Ref<Weather | null | void> = ref(null)
-const errorMessage: Ref<String | null> = ref("")
+const results: Ref<Weather | null> = ref(null)
+const errorMessage: Ref<String> = ref('')
 
 async function getWeather(location: string): Promise<void> {
-        try {
-            const response = await ResultService.getWeather(location)
-            results.value = response
-            errorMessage.value = null
-        } catch (error) {
-            results.value = null
-
-            if (error instanceof Error) {
-                switch (error.name) {
-                    case "InvalidLocationError":
-                        errorMessage.value = "Please enter a valid location"
-                        break;
-                    }
-            }      
+    try {
+        const response = await ResultService.getWeather(location)
+        results.value = response
+        errorMessage.value = ''
+    } catch (error: unknown) {
+        switch ((error as Error).name) {
+            case 'InvalidLocation':
+                errorMessage.value = 'Please enter a valid location'
+                break
+            default:
+                errorMessage.value = 'Something went wrong, please try again later'
         }
+        results.value = null
+    }
 }
-
 </script>
 
 <template>
     <h2>How's the weather?</h2>
-    <SearchBar @input="getWeather" :message="errorMessage"/>
+    <SearchBar @input="getWeather" :message="errorMessage" />
     <SearchResult :weather="results" />
-
-</template> 
+</template>
 
 <style scoped>
 /* Tablet Breakpoint */
